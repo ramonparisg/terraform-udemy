3396f57cbc0d078de3c217135a1f67c59916143e
Ramon Paris <pariis78@gmail.com>
2019-06-05 17:51:42 -0400

    Demo 2 - Agregando una Keypair generada y subiendo un archivo directo

--- instance.tf
+++ instance.tf
@@ -1,4 +1,24 @@
+resource "aws_key_pair" "ramon-key" {
+  key_name   = "mykey"
+  public_key = "${file(var.PATH_TO_PUBLIC_KEY)}"
+}
+
 resource "aws_instance" "example" {
   ami           = "${var.AMIS[var.AWS_REGION]}"
   instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.ramon-key.key_name}"
+
+
+  provisioner "file" {
+    source      = "ramon.txt"
+    destination = "~/ramon.txt"
+  }
+
+  connection {
+    host        = "${coalesce(self.public_ip, self.private_ip)}"
+    type        = "ssh"
+    user        = "${var.INSTANCE_USERNAME}"
+    private_key = "${file(var.PATH_TO_PRIVATE_KEY)}"
+  }
+
 }
--- vars.tf
+++ vars.tf
@@ -14,4 +14,15 @@ variable "AMIS" {
   }
 }
 
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
 
